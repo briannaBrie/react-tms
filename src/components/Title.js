64640d005a3a235363a757f8909f60ba8def5609
brianneBrie <bridgetmukuhi@gmail.com>
2021-05-23 08:18:01 +0200

    First Level of the app

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
         <div className = "title">
             <h1>The Missing Slice</h1>
             <h2>Gallery</h2>
-            <h3>All you need is Love\n But a little chocolate now and then doesn't hurt</h3>
+            <h3>All you need is Love. But a little chocolate now and then doesn't hurt</h3>
         </div>
     )
 }
