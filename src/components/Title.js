2b97402c23484c2e1864c7029dd033ccf0ec43eb
brianneBrie <bridgetmukuhi@gmail.com>
2021-05-23 07:55:32 +0200

    Initial Commit

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Title = () => {
+    return (
+        <div className = "title">
+            <h1>The Missing Slice</h1>
+            <h2>Gallery</h2>
+            <h3>All you need is Love\n But a little chocolate now and then doesn't hurt</h3>
+        </div>
+    )
+}
+
+export default Title
